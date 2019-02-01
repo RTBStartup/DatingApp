3fc5a94ec1a046960dfdced2a9fc47ba7194cf86
Taiyab Husain <startup.rtb@gmail.com>
2019-02-01 16:31:44 +0530

    End of Section 11

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './_services/auth.service';
 import {JwtHelperService} from '@auth0/angular-jwt';
+import { User } from './_models/user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,6 +14,13 @@ constructor(private authoService: AuthService) {}
 
 ngOnInit() {
   const token = localStorage.getItem('token');
+  const user: User = JSON.parse(localStorage.getItem('user'));
+  if (token) {
   this.authoService.decodedToken = this.jwtHelper.decodeToken(token);
-}
+  }
+  if (user) {
+    this.authoService.currentUser = user;
+    this.authoService.changeMemberPhoto(user.photoUrl);
+  }
+ }
 }
--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
@@ -18,12 +19,13 @@ namespace DatingApp.API.Controllers
     {
         private readonly IAuthRepositoty _repo;
         private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
 
-        public AuthController(IAuthRepositoty repo, IConfiguration config)
+        public AuthController(IAuthRepositoty repo, IConfiguration config, IMapper mapper)
         {
             _repo = repo;
             _config = config;
-            
+            _mapper = mapper;
         }
         [HttpPost("login")]
          public async Task<IActionResult> Login(UserForRegisterDto userForRegisterDto)
@@ -49,9 +51,12 @@ namespace DatingApp.API.Controllers
            var tokenHandler = new JwtSecurityTokenHandler();
 
            var token = tokenHandler.CreateToken(tokenDescriptor);
+           
+           var user = _mapper.Map<UserForListDto>(userFromRepo);
 
            return Ok( new {
-               token = tokenHandler.WriteToken(token)
+               token = tokenHandler.WriteToken(token),
+               user
            });
         
         }
